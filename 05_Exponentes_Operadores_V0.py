398bdfd36e8cd1951eaf2de83d66123a197a2e05
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-20 13:25:39 -0600

    Operadores

---
+++ 05_Exponentes_Operadores_V0.py
@@ -0,0 +1,14 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+#Calcular Exponentes
+
+#Para exponentes se usa "**"
+
+ejemplo = 5 ** 2 #Para probarlo elevamos el numero 5 a la 2
+print(ejemplo)#corroboramos resultado
+
+practicando = 5 ** 2 + 77 / 10 #Para probarlo mezclamos varios operadore aritmeticos a ver que sucede
+print(practicando)#corroboramos resultado
+
+#Ejercicio
+elev1024 = 2 ** 10 #Nos pide elevar el numero 2 para que nos de 1024
+print(elev1024)#corroboramos resultado
