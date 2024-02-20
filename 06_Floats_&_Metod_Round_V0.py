42f74978c5bbafcad273617eed4f86e21f6c1968
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-20 13:26:02 -0600

    Floats y métodos

---
+++ 06_Floats_&_Metod_Round_V0.py
@@ -0,0 +1,25 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+#Floats y metodo round()
+
+#-> Float
+
+#Los floats se representan con un punto
+Axo = 5.7 #Almacenamos  floats en dos variables
+Axo2 = 7.5 
+print(Axo+Axo2)#corroboramos resultado
+
+
+#-> Metodo round()
+
+#Sirve para redondear los decimales de un float
+Axo = 5.7777777 #Almacenamos  floats en dos variables
+Axo2 = 7.5555555
+result = Axo+Axo2 #Almacenamos el resultado de la suma en una variable
+print(round(result,3))#Le pedimos que solo dejara 3 decimales
+
+#Ejercicio
+
+#Nos pide redondear con decimales un numero dado
+
+numraro = 17.567383292929200234 #Almacenamos el valor que se nos dio en una variable
+print(round(numraro,5))#corroboramos que lo redondee correctamente
