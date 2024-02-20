f8ad5757ce5085d8d17ff4fc3849d2c7a00f74b6
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-20 13:25:19 -0600

    Operadores

---
+++ 04_Suma_Resta_Multi_Divis_Operadores_V0.py
@@ -0,0 +1,41 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+#Suma, resta, multiplicación y división
+
+#integers
+
+sumar = 7 + 9 + 5
+restar = 7 - 9 - 5
+multiplicar = 7 * 9 * 5
+dividir = 7 / 9 / 5
+#Arriba sumamos, restamos, multiplicamos y dividimos individualmente los 3 mismos numeros
+print("Suma: ", sumar, "\nResta: " , restar , "\nMultiplicacion: " , multiplicar , "\nDivision: " , dividir)
+#Imprimimos todos los resultados, usamos coma para concatenar ya que si usamos + se podrian generar errores al sumar numeros y strings
+
+combinados = 7 + (9 / 5) - (3 * 4)
+print("Resultado de operacion 7 + 9 / 5 - 3 * 4: ", combinados)
+#Hacemos so de multiples simbolos aritmeticos y parentesis
+
+#Ejercicios
+
+#1
+sumar87 = 20 + 23 + 7 + 17 + 8 + 2 + 10 #Nos pideun una suma cuyo resultado sea 87 y debe contener 20 y 23
+print("Da 87?: ", sumar87)#Corroboramos resultado
+
+#2
+restar87 = - 20 - 23 - 7 - 17 - 8 - 2 - 10 #Nos pideun una resta cuyo resultado sea -87 y debe contener 20 y 23
+print("Da -87?: ", restar87)#Corroboramos resultado
+
+#3
+multi870 = 20 * 23 + 10 * 41 #Nos pideun una multiplicacion cuyo resultado sea 870 y debe contener 20 y 23
+print("Da 870?: ", multi870)#Corroboramos resultado
+
+#4
+divi10 = ((5000 / 10)/ 230)* 5 #Nos pideun una division cuyo resultado sea 10 , puede tener decimales y debe contener 5000 y 230
+print("Da 10?... puede tener decimales: ", divi10)#Corroboramos resultado
+
+#5
+operacionrara = (10 + 5 - 15) / 17 #Primera opcion planteada
+opc2 =10 / 5 + 15 - 17 #Segunda opcion planteada sin parentesis
+#Nos dieron los numeros y debiamos de elegir 3 simbolos aritmeticos para que el resultado diera 0
+print("Da 0?: ", operacionrara)#Corroboramos resultado
+print("Da 0?: ", opc2)#Corroboramos resultado 2
