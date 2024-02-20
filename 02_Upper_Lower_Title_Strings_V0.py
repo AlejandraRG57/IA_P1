8877c71f68f6f4a4e87e6aca2f1576c98cbdaeaf
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-20 12:09:56 -0600

    Strings

---
+++ 02_Upper_Lower_Title_Strings_V0.py
@@ -0,0 +1,23 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+#Los metodos upper(), lower() y title()
+
+#Metodo title() -> Hace que la primera letra de cada palabra del string este en mayuscula
+
+titulo = "feliz navidad con title" #Le damos un string a la variable titulo
+titulo = titulo.title() #A la variable titulo le decimos que es ella misma pero asignandole un .title() despues
+print(titulo) #Imprimimos la variable titulo
+
+#Metodo title() de forma mas breve
+
+titulo = "feliz navidad con title mas breve".title() #Aqui asignamos el string a la variable titulo y a su vez le damos formato con .title()
+print(titulo) #Imprimimos la variable titulo
+
+#Metodo upper() -> Hace que todas las letras de cada palabra del string esten en mayusculas
+
+titulo = "feliz navidad con upper".upper() #Aqui asignamos el string a la variable titulo y a su vez le damos formato con .upper()
+print(titulo) #Imprimimos la variable titulo
+
+#Metodo lower() -> Hace que todas las letras de cada palabra del string esten en minusculas
+
+titulo = "FELIZ NAVIDAD CON LOWER".lower() #Aqui asignamos el string a la variable titulo y a su vez le damos formato con .lower()
+print(titulo) #Imprimimos la variable titulo
