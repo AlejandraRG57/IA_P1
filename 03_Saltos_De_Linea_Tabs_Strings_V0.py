1d28d1bf4d0881a8d60456d70e827b23c28243f8
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-20 12:24:57 -0600

    Strings

---
+++ 03_Saltos_De_Linea_Tabs_Strings_V0.py
@@ -0,0 +1,18 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+#Saltos de linea y tabulaciones
+
+
+#->Saltos de linea
+
+print("Hola\nmundo\nfeliz\nnavidad")
+#El \n sirve para hacer saltos de linea como si dieramos un enter cada vez que lo ponemos
+
+#->Tabulacion
+
+print("Razones por las que tronare control:\n\t- Profesor\n\t- No formulario\n\t- Profesor x2\n\t- La materia es dificil de por si\n\t- El profesor habla bien bajito y estoy medio sorda\n\t- Profesor x1000")
+#Usamos el \t para dar tabulaciones en el texto
+
+#->Ejercicio
+
+print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.\n-HTML.\n-C#.\n-Rust.")
+#Hacemos una lista de lenguajes de programacion con un salto de linea despues de cada palabra
