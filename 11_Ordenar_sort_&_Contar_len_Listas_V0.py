ea109fea1e705b664a90d3ccd37f6ba9f3868eb4
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-22 11:07:56 -0600

    Listas

---
+++ 11_Ordenar_sort_&_Contar_len_Listas_V0.py
@@ -0,0 +1,31 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+
+# -> Ordenar elementos con sort() y sorted()
+print("\n\t -> Ordenar elementos con sort() y sorted()\n")#Le damos un titulo
+
+cantantes = ["Camila Cabello", "Katy Perry", "Eminem", "Morat", "Paulo Londra"]#Creamos una lista
+cantantes.sort()#Utilizamos el metodo sort
+print(cantantes)#Verificamos que se hayan ordenado
+
+cantantes.sort(reverse=True)#Utilizamos el metodo sort pero inverso
+print(cantantes)#Verificamos que se hayan invertido
+print(cantantes[1])#Imprimimos el elemento que este en la posicion 1, el cual es morat al haber sido ordenado inversamente
+
+vengadores = ["Iron Man","Black Widow", "Capitan America", "Hulk","Hawkeye"]#Creamos otra lista
+print(sorted(vengadores))#Utilizamos el metodo sorted para que sea temporal su ordenamiento
+print(vengadores)#Verificamos que no esten ordenados
+
+#Ejercicio 1
+#Nos piden ordenas esta lista en orden alfabetico inverso
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.sort(reverse=True)#Utilizamos el metodo sort pero inverso
+print(colores)#Verificamos que este correcto
+
+
+# -> Contar elementos con len()
+print("\n\t -> Contar elementos con len()\n")#Le damos un titulo
+
+marvel = ["Kate Bishop", "Wanda Maximof", "Natasha Romanoff", "Yelena Belova", "Carol Danvers", "Kamala Khan"]#Creamos otra lista
+print(len(marvel))#Utilizamos el metodo len para contar los elementos y lo imprimimos
+cantidad = len(marvel) #Almacenamos cuantos personajes hay en una variable
+print("Son",cantidad,"increibles vengadores que conforman nuestra lista")#Probamos usar el dato que guardamos en una variable
