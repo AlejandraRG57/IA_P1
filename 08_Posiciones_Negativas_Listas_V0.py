dd61808f35546cb1ba20fdfc6d3e26579ce69a07
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-21 23:22:35 -0600

    Listas

---
+++ 08_Posiciones_Negativas_Listas_V0.py
@@ -0,0 +1,19 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+#Posiciones negativas en listas
+
+#Sirven para empezar a contar a la inversa para acceder a elementos desde el final de la lista
+
+marvel = ["Kate Bishop", "Wanda Maximof", "Natasha Romanoff", "Yelena Belova", "Carol Danvers", "Kamala Khan"]#Hacemos una lista bastante larga
+print(marvel)#Imprimimos la lista completa
+print(marvel[-3])#Imprimimos contando desde atras 
+
+#Ejercicio
+#Tenemos que acceder a varios colores de la lista utlizando las posiciones negativas
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']#Escribimos la lista que nos plantearon
+print(colores)#Imprimimos la lista completa
+print(colores[-1])#Accedemos al color naranja
+print(colores[-7])#Accedemos al color amarillo
+print(colores[-5])#Accedemos al color lila
+print(colores[-2])#Accedemos al color blanco
+print(colores[-10])#Accedemos al color rojo
