d865a9cce941a0fce45aa27e404c7c435c591740
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-21 23:21:30 -0600

    Listas

---
+++ 07_Crear_Listas_V0.py
@@ -0,0 +1,34 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+#Crear y Utilizar listas
+
+#Asi se escribe una lista
+
+vengadores = ["Iron Man","Black Widow", "Capitan America", "Hulk","Hawkeye"] #Planteamos la lista
+print(vengadores) #Imprimimos todo el contenido de la lista
+print(vengadores[1]) #Podemos imprimir un elemento especifico de la lista, se empieza a contar a partir del 0.
+
+#Tambien podemos mezclar numeros y textos
+numeros = [ 55, 'nueve', 77,'siete'] #Mezclamos numeros y texto en esta lista
+print(numeros) #Imprimimos todo el contenido de la lista
+print(numeros[3])#Probamos otra vez el imprimir un elemento en especifico
+
+
+
+#Ejercicios
+
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(colores)#Imprimimos toda la lista
+print(colores[3]) #El amarillo esta en la posicion 3
+print(colores[0]) #El rojo se encuentra en la posicion 0
+print(colores[7]) #El rosa se encuentra en las posicion 7
+
+#Nos pide crear una lista con las siuientes especificaciones:
+# "uno" en la posición 4 , "dos" en la posición 1 , "tres" en la posición 0
+# "cuatro" en la posición 3 , "cinco" en la posición 2
+lista = ["tres", "dos", "cinco", "cuatro", "uno"]
+print(lista) #Imprimimos toda la lista
+print(lista[0]) #Corroboramos que la lista este escrita correctamente
+print(lista[1]) #Corroboramos que la lista este escrita correctamente
+print(lista[2]) #Corroboramos que la lista este escrita correctamente
+print(lista[3]) #Corroboramos que la lista este escrita correctamente
+print(lista[4]) #Corroboramos que la lista este escrita correctamente
