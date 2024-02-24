3011f045e6e9b9f111c04280c321f10b3ab82a3a
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-23 23:45:29 -0600

    Condicionales

---
+++ 13_IF_Condicionales_V0.py
@@ -0,0 +1,61 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+
+# -> Condicional If y Comparadores
+
+var1 = 7 #Planteamos esta primera variable para todas nuestras comparaciones
+var2 = 5 #Planteamos esta segunda variable tambien para todas nuestras comparaciones
+var3 = 7 #Planteamos esta tercera variable tambien para todas nuestras comparaciones
+
+#Usando >
+
+if var2 > var1: #Usamos un ejemplo donde no se cumple el if con el uso de >
+    print("Si lees esto algo esta mal con el uso de >") #Si llegara a entrar al if se imprimiria eso
+
+if var3 > var2: #Usamos un ejemplo donde si se cumple el if con el uso de >
+    print("Si lees esto todo esta bien con el uso de >") #Si llegara a entrar al if se imprimiria eso
+    
+#Usando <
+
+if var3 < var2: #Usamos un ejemplo donde no se cumple el if con el uso de <
+    print("Si lees esto algo esta mal con el uso de <") #Si llegara a entrar al if se imprimiria eso
+
+if var2 < var1: #Usamos un ejemplo donde si se cumple el if con el uso de <
+    print("Si lees esto todo esta bien con el uso de <") #Si llegara a entrar al if se imprimiria eso
+
+#Usando >=
+    
+if var2 >= var1: #Usamos un ejemplo donde no se cumple el if con el uso de >=
+    print("Si lees esto algo esta mal con el uso de >=") #Si llegara a entrar al if se imprimiria eso
+
+if var2 >= var2: #Usamos un ejemplo donde si se cumple el if con el uso de >=
+    print("Si lees esto todo esta bien con el uso de >=") #Si llegara a entrar al if se imprimiria eso
+
+if var1 >= var2: #Usamos otro ejemplo donde si se cumple el if con el uso de >=
+    print("Si lees esto todo esta bien con el uso de >=") #Si llegara a entrar al if se imprimiria eso
+
+#Usando <=
+
+if var1 <= var2: #Usamos un ejemplo donde no se cumple el if con el uso de <=
+    print("Si lees esto algo esta mal con el uso de <=") #Si llegara a entrar al if se imprimiria eso
+
+if var2 <= var2: #Usamos un ejemplo donde si se cumple el if con el uso de <=
+    print("Si lees esto todo esta bien con el uso de <=") #Si llegara a entrar al if se imprimiria eso
+
+if var2 <= var1: #Usamos otro ejemplo donde si se cumple el if con el uso de <=
+    print("Si lees esto todo esta bien con el uso de <=") #Si llegara a entrar al if se imprimiria eso
+
+#Usando ==
+
+if var1 == var2: #Usamos un ejemplo donde no se cumple el if con el uso de ==
+    print("Si lees esto algo esta mal con el uso de ==") #Si llegara a entrar al if se imprimiria eso
+
+if var1 == var3: #Usamos un ejemplo donde si se cumple el if con el uso de ==
+    print("Si lees esto todo esta bien con el uso de ==") #Si llegara a entrar al if se imprimiria eso
+
+#Usando !=
+
+if var2 != var2: #Usamos un ejemplo donde no se cumple el if con el uso de !=
+    print("Si lees esto algo esta mal con el uso de !=") #Si llegara a entrar al if se imprimiria eso
+
+if var1 != var2: #Usamos un ejemplo donde si se cumple el if con el uso de !=
+    print("Si lees esto todo esta bien con el uso de !=") #Si llegara a entrar al if se imprimiria eso
