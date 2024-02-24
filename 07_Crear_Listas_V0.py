db89f3f39b0c9d0c7ccf38252fcd920af70dce45
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-02-23 22:32:57 -0600

    Listas

--- 07_Crear_Listas_V0.py
+++ 07_Crear_Listas_V0.py
@@ -16,16 +16,16 @@ print(numeros[3])#Probamos otra vez el imprimir un elemento en especifico
 
 #Ejercicios
 
-colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]#Nos da una lista de colores el curso y nos pide decir que color esta en la posicion num 3, e indicar donde esta el color rojo y rosa
 print(colores)#Imprimimos toda la lista
 print(colores[3]) #El amarillo esta en la posicion 3
 print(colores[0]) #El rojo se encuentra en la posicion 0
 print(colores[7]) #El rosa se encuentra en las posicion 7
 
-#Nos pide crear una lista con las siuientes especificaciones:
+#Nos pide crear una lista con las siguientes especificaciones:
 # "uno" en la posición 4 , "dos" en la posición 1 , "tres" en la posición 0
 # "cuatro" en la posición 3 , "cinco" en la posición 2
-lista = ["tres", "dos", "cinco", "cuatro", "uno"]
+lista = ["tres", "dos", "cinco", "cuatro", "uno"]#Planteamos una posible solucion a lo mencionado arriba
 print(lista) #Imprimimos toda la lista
 print(lista[0]) #Corroboramos que la lista este escrita correctamente
 print(lista[1]) #Corroboramos que la lista este escrita correctamente
