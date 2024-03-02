ad3e6bbc92cbd73840e9fd629ae8efcb20f11846
Alejandra Rodríguez Guevara <160206818+AlejandraRG57@users.noreply.github.com>
2024-03-01 23:34:57 -0600

    Control de errores

---
+++ 25_Manejo_Excepciones_Control de Errores_V0.py
@@ -0,0 +1,12 @@
+#Alejandra Rodriguez Guevara 21310127 6E1
+
+# -> TEMA: Manejo de excepciones
+print("\n -> Manejo de excepciones \n")#Explicamos lo que hara el codigo brevemente con un print
+
+var1= "Afirmativo compañero" #declaramos una variable
+
+try: #Try busca herrores en el codigo que le presentes y ejecuta en caso de que no haya
+	print(var1)#Pedimos que imprima una variable
+	print(var2)#Pedimos que imprima una variable
+except: #Except es la respuesta que el codigo tendra en caso de que haya errores.
+	print("Demonios chavo, te equivocaste en algo") #En caso de que no este la variable imprime esto
